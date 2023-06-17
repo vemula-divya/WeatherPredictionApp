c5256a2f89ab4b8a12dbcdeb62168b34206aa2bc
vemula-divya <vemuladivya2712@gmail.com>
2023-06-16 18:55:29 -0700

    Sample Code Running and Displaying Index page

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for, request, jsonify
+from flask_cors import cross_origin
+
+app = Flask(__name__, template_folder="frontend")
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
